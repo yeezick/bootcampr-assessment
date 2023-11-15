9f48aa037c538c6ff2e085dafb03fe76f9a385f9
GeoSauer <geoffrey.sauer89@gmail.com>
2023-11-15 14:01:14 -0700

    add basic layout of SignUp page

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -1,5 +1,19 @@
+import { Box, Container, Grid, Stack } from '@mui/material'
 import './SignUp.scss'
+import SignUpImage from '../../assets/signup-image.png'
 
 export default function SignUp() {
-  return <div>SignUp</div>
+  return (
+    <Container>
+      <Stack alignItems='center'>
+        <h1>Join Bootcampr today.</h1>
+        <h3>Get the experience. Get the job.</h3>
+      </Stack>
+      <Grid>
+        <Box>
+          <img src={SignUpImage} />
+        </Box>
+      </Grid>
+    </Container>
+  )
 }
