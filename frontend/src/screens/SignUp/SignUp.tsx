fa4a683ff14b420b93ad4d74fd4538148ea4c6ba
prateek <prateeksingh8841@gmail.com>
2023-11-14 21:36:17 -0700

    feat: sign up

---
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SignUp.scss'
+
+export const SignUp: React.FC = () => {
+  return (
+    <div className='signup-container'>
+          <h1>Join Bootcampr today.</h1>
+    </div>
+  )
+}
