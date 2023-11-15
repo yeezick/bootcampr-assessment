216c9cc7fc6fa748128b72fb6e77940a33510465
prateek <prateeksingh8841@gmail.com>
2023-11-15 00:02:42 -0700

    feat: css

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import Logo from '../../assets/SignUpImage.svg';
 import './SignUp.scss'
 
 export const SignUp: React.FC = () => {
   return (
     <div className='signup-container'>
-      <div className='top-text'>
+      {/* <div className='top-container'>
         <h1 className="first">Join Bootcampr today.</h1>
         <h1 className="second">Get the experience. Get the job.</h1>
+      </div> */}
+      <div className='bottom-container'>
+        <img src={ Logo } className='signup-image' />
+        <div>Form Content</div>
       </div>
     </div>
   )
