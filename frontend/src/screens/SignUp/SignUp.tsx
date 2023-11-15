c86a1e3eacb0c3cb3605ab6bf1a5c5cf5f01f5ed
prateek <prateeksingh8841@gmail.com>
2023-11-14 23:47:40 -0700

    feat: css

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -4,7 +4,10 @@ import './SignUp.scss'
 export const SignUp: React.FC = () => {
   return (
     <div className='signup-container'>
-          <h1>Join Bootcampr today.</h1>
+      <div className='top-text'>
+        <h1 className="first">Join Bootcampr today.</h1>
+        <h1 className="second">Get the experience. Get the job.</h1>
+      </div>
     </div>
   )
 }
