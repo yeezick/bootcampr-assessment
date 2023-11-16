b2e1f2f9cae01fee8fde5fdf1d00a7b576448eb8
Vanessa Wharton <vanessa.primetime@gmail.com>
2023-11-16 02:05:50 -0600

    Input basic form text and began UI

--- frontend/src/screens/Register/Register.tsx
+++ frontend/src/screens/Register/Register.tsx
@@ -1,43 +1,33 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Button } from '@mui/material'
-import { sayHello } from 'utils/sampleController'
+import { Box, Checkbox, FormControl, FormControlLabel, TextField, Button } from '@mui/material'
 import './Register.scss'
 
 export const Register: React.FC = () => {
-const [helloResponse, setHelloResponse] = useState<string>('')
 
 return (
-    <div className='landing-container'>
-    <div className='header-container'>
-        <div className='header-grid'>
-        <ButtonContainer setHelloResponse={setHelloResponse} />
-        {helloResponse !== '' && helloResponse}
+        <div className='register-container'>
+            <h1>
+                Join Bootcampr today.
+            </h1>
+            <h3>
+                Get the experience. Get the job.
+            </h3>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': {m:1},
+                }}
+            >
+                <FormControl variant="standard">
+                    <TextField id="filled" label="First name" variant="filled"/>
+                    <TextField id="filled" label="Last name" variant="filled"/>
+                    <TextField required id="filled" label="Email address (ex. jeanine@bootcampr.io)" variant="filled"/>
+                    <TextField id="filled-password-input" label="Password" type="password" variant="filled" />
+                    <TextField id="filled-password-reentered-input" label="Re-Enter Password" type="password" variant="filled" />
+                    <FormControlLabel control={<Checkbox />} label="I agree to receive email notification(s). We will only send emails with important information, like project start dates. We will not sell your information!" />
+                    <Button variant="contained">Sign up</Button>
+                </FormControl>
+            </Box>
         </div>
-    </div>
-    </div>
-)
-}
-
-const ButtonContainer = ({ setHelloResponse }) => {
-    const navigate = useNavigate()
-    const handleSignUpButton = () => {
-    navigate('/sign-up')
-    }
-
-    const handleApiTestButton = async () => {
-    const apiResponse = await sayHello()
-    setHelloResponse(apiResponse)
-    }
-
-return (
-    <div className='button-container'>
-    <Button onClick={handleSignUpButton} variant='contained'>
-        Start Today!
-    </Button>
-    <Button onClick={handleApiTestButton} variant='contained'>
-        Test the API
-    </Button>
-    </div>
 )
 }
