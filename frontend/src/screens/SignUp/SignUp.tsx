474d5d3273bf21340ee5e7012d82683433bf4537
prateek <prateeksingh8841@gmail.com>
2023-11-15 21:20:44 -0700

    feat: css

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import Logo from '../../assets/SignUpImage.svg';
+import { TextField } from '@mui/material'
+import Logo from '../../assets/SignUpImage.svg'
 import './SignUp.scss'
 
 export const SignUp: React.FC = () => {
   return (
     <div className='signup-container'>
       <div className='top-container'>
-        <h1 className="first">Join Bootcampr today.</h1>
-        <h1 className="second">Get the experience. Get the job.</h1>
+        <h1 className='first'>Join Bootcampr today.</h1>
+        <h1 className='second'>Get the experience. Get the job.</h1>
       </div>
       <div className='bottom-container'>
-        <img src={ Logo } className='signup-image' />
-        <div>Form Content</div>
+        <img src={Logo} className='signup-image' />
+        <div className='form-content'>
+          <TextField helperText=' ' label='First Name' />
+          <TextField helperText=' ' label='Last Name' />
+        </div>
       </div>
     </div>
   )
