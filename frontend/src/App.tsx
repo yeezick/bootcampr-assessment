349eccc836b678dfd8610bec347ab82748ff1ada
rica213 <25f5k252l@mozmail.com>
2023-11-17 01:17:17 +0300

    Add sign-up to the route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Landing } from './screens/Landing/Landing'
 import { Route, Routes } from 'react-router'
 import { Layout } from './layout/Layout'
+import { SignUp } from 'screens/SignUp/SignUp'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path='/' element={<Landing />} />
+          <Route path='/sign-up' element={<SignUp />} />
         </Routes>
       </Layout>
     </>
