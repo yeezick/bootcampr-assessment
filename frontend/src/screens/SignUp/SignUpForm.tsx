d97fa1f08452ef25f17a4a97e86dd9385ef6e8ad
rica213 <25f5k252l@mozmail.com>
2023-11-17 01:01:28 +0300

    Implement the Sign up form component

---
+++ frontend/src/screens/SignUp/SignUpForm.tsx
@@ -0,0 +1,135 @@
+import React, { useState } from 'react'
+import './SignUpForm.scss'
+import {
+  Button,
+  TextField,
+  Box,
+  InputAdornment,
+  IconButton,
+  OutlinedInput,
+  InputLabel,
+  FormControlLabel,
+  Checkbox,
+} from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+
+
+
+const SignUpForm: React.FC = () => {
+  const emailNotificationAgreement =
+    'I agree to receive email notification(s). We will only send emails with important information, like project start dates. We will not sell your information!'
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const [passwordState, setPasswordState] = useState({
+    showPassword: false,
+  })
+
+  const [confirmPasswordState, setConfirmPasswordState] = useState({
+    showConfirmPassword: false,
+  })
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    // TODO: handle submit
+  }
+  
+  const handleInputChange = (field: string, value: string) => {
+    setFormData({ ...formData, [field]: value})
+  }
+
+  const handleTogglePasswordVisibility = () => {
+    setPasswordState({
+      ...passwordState,
+      showPassword: !passwordState.showPassword,
+    })
+  }
+
+  const handleToggleConfirmPasswordVisibility = () => {
+    setConfirmPasswordState({
+      ...confirmPasswordState,
+      showConfirmPassword: !confirmPasswordState.showConfirmPassword,
+    })
+  }
+
+
+  return (
+    <Box component='form' onSubmit={handleSubmit} className='form'>
+      <InputLabel htmlFor='firstName'>First name</InputLabel>
+      <TextField required id='firstName' name='firstName' autoFocus />
+      <InputLabel htmlFor='lastName'>Last name</InputLabel>
+      <TextField required id='lastName' name='lastName' autoFocus />
+      <InputLabel htmlFor='email'>
+        Email address (ex. jeanine@bootcampr.io)
+      </InputLabel>
+      <TextField
+        required
+        id='email'
+        name='email'
+        type='email'
+        autoComplete='email'
+        autoFocus
+      />
+      <InputLabel htmlFor='password'>
+        Password (Minimum 8 characters, 1 uppercase, 1 lowercase, 1 symbol)
+      </InputLabel>
+      <OutlinedInput
+        required
+        name='password'
+        id='password'
+        autoComplete='current-password'
+        type={passwordState.showPassword ? 'text' : 'password'}
+        value={formData.password}
+        onChange={e => handleInputChange('password', e.target.value)}
+        endAdornment={
+          <InputAdornment position='end'>
+            <IconButton
+              aria-label='toggle password visibility'
+              onClick={handleTogglePasswordVisibility}
+              edge='end'
+            >
+              {passwordState.showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+      <InputLabel htmlFor='confirmPassword'>Re-enter password</InputLabel>
+      <OutlinedInput
+        required
+        name='confirmPassword'
+        id='confirmPassword'
+        autoComplete='current-password'
+        type={confirmPasswordState.showConfirmPassword ? 'text' : 'password'}
+        value={formData.confirmPassword}
+        onChange={e => handleInputChange('confirmPassword', e.target.value)}
+        endAdornment={
+          <InputAdornment position='end'>
+            <IconButton
+              aria-label='toggle password visibility'
+              onClick={handleToggleConfirmPasswordVisibility}
+              edge='end'
+            >
+              {confirmPasswordState.showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+      <FormControlLabel
+        className='email-agreement checkbox-email-agreement'
+        control={<Checkbox value='remember' />}
+        label={emailNotificationAgreement}
+      />
+      <Button type='submit' className='submit-button'>
+        Sign up
+      </Button>
+    </Box>
+  )
+}
+
+export default SignUpForm
