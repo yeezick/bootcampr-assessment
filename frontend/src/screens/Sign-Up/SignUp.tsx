6b0dd6ef4d7ea35fbfb609e580044c6776d9160b
Jiecheng Dong <jiedong111@gmail.com>
2023-11-16 09:07:04 -0500

    Initial routing of sign up page

---
+++ frontend/src/screens/Sign-Up/SignUp.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button } from '@mui/material'
+import { sayHello } from 'utils/sampleController'
+import './SignUp.scss'
+
+export const SignUp: React.FC = () => {
+  
+    return (
+      <div>Hello</div>
+    )
+  }
