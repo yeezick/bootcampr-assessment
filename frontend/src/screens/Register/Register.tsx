2d5a774af2f1d6d7cb6eec1352fc961e56ba027b
Vanessa Wharton <vanessa.primetime@gmail.com>
2023-11-16 02:42:37 -0600

    Added Register Page image and password visibility icon and event handlers

--- frontend/src/screens/Register/Register.tsx
+++ frontend/src/screens/Register/Register.tsx
@@ -1,8 +1,30 @@
-import React, { useState } from 'react'
-import { Box, Checkbox, FormControl, FormControlLabel, TextField, Button } from '@mui/material'
+import React from 'react'
+import { Box, Button, Checkbox, FormControl, FormControlLabel, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, Paper, TextField } from '@mui/material'
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
 import './Register.scss'
 
 export const Register: React.FC = () => {
+    // const handleRegister = (event: React.FormEvent<HTMLFormElement>) => {
+
+    //     event.preventDefault();
+    //     const data = new FormData(event.currentTarget);
+    //     console.log({
+    //         firstname: data.get('firstname'),
+    //         lastname: data.get('lastname'),
+    //         email: data.get('email'),
+    //         password: data.get('password'),
+    //         checkbox: data.get('checkbox'),
+    //     });
+    // };
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
 
 return (
         <div className='register-container'>
@@ -12,22 +34,62 @@ return (
             <h3>
                 Get the experience. Get the job.
             </h3>
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': {m:1},
-                }}
-            >
-                <FormControl variant="standard">
-                    <TextField id="filled" label="First name" variant="filled"/>
-                    <TextField id="filled" label="Last name" variant="filled"/>
-                    <TextField required id="filled" label="Email address (ex. jeanine@bootcampr.io)" variant="filled"/>
-                    <TextField id="filled-password-input" label="Password" type="password" variant="filled" />
-                    <TextField id="filled-password-reentered-input" label="Re-Enter Password" type="password" variant="filled" />
-                    <FormControlLabel control={<Checkbox />} label="I agree to receive email notification(s). We will only send emails with important information, like project start dates. We will not sell your information!" />
-                    <Button variant="contained">Sign up</Button>
-                </FormControl>
-            </Box>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: '../assets/RegisterPageImage.png',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <Box
+                        component="form"
+                        // onSubmit={handleRegister}
+                        sx={{
+                        my: 8,
+                        mx: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'left',
+                        }}
+                    >
+                        <FormControl variant="standard">
+                            <TextField id="filled" label="First name" variant="filled"/>
+                            <TextField id="filled" label="Last name" variant="filled"/>
+                            <TextField required id="filled" label="Email address (ex. jeanine@bootcampr.io)" variant="filled"/>
+                            <TextField id="filled-password-input" label="Password" type="password" variant="filled">
+                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-password"
+                                    type={showPassword ? 'text' : 'password'}
+                                    endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                        >
+                                        {/* {showPassword ? <VisibilityOff /> : <Visibility />} */}
+                                        </IconButton>
+                                    </InputAdornment>
+                                    }
+                                    label="Password"
+                                />
+                            </TextField>
+                            <TextField id="filled-password-reentered-input" label="Re-Enter Password" type="password" variant="filled" />
+                            <FormControlLabel control={<Checkbox />} label="I agree to receive email notification(s). We will only send emails with important information, like project start dates. We will not sell your information!" />
+                            <Button variant="contained">Sign up</Button>
+                        </FormControl>
+                    </Box>
+                </Grid>
+            </Grid>
         </div>
 )
 }
