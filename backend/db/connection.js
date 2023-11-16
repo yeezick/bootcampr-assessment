6aefd31b8f8c347aa422686ce6ce2c57ff89098f
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-11-16 14:19:06 +0000

    Add signup scss and tsx template, link route in App.tsx

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import "dotenv/config.js";
 
 // Define your connection URI here
-// const MONGODB_URI =
+const MONGODB_URI = 'mongodb+srv://admin-jessica:rjq.dnx%40ZRU-wyw1dzr@bootcamprcluster.qw8w2ox.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.set("strictQuery", false);
 mongoose.set("returnOriginal", false); //for findByAndUpdate to return a reference to object at location
---
+++ frontend/src/screens/Signup/Signup.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { Button } from '@mui/material'
+import './Signup.scss'
+
+export const Signup: React.FC = () => {
+
+  return (
+    <div className=''>
+      <div className=''>
+        <div className=''>
+        </div>
+      </div>
+    </div>
+  )
+}
