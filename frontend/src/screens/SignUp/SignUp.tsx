e92ea5db7860231e0fdc1e37e9fa0e452556ae1e
prateek <prateeksingh8841@gmail.com>
2023-11-15 21:06:27 -0700

    feat: css

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -5,10 +5,10 @@ import './SignUp.scss'
 export const SignUp: React.FC = () => {
   return (
     <div className='signup-container'>
-      {/* <div className='top-container'>
+      <div className='top-container'>
         <h1 className="first">Join Bootcampr today.</h1>
         <h1 className="second">Get the experience. Get the job.</h1>
-      </div> */}
+      </div>
       <div className='bottom-container'>
         <img src={ Logo } className='signup-image' />
         <div>Form Content</div>
