3df9ad60fd56e1a6fee1d8507ed6e8a31f779755
GeoSauer <geoffrey.sauer89@gmail.com>
2023-11-17 14:13:33 -0700

    user controller

---
+++ backend/controllers/user.js
@@ -0,0 +1,11 @@
+import User from "../models/user";
+
+export const postUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).send(newUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
