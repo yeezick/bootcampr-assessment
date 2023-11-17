27c063346c9c02ac776cf3e3e04799ac535b1afa
ozge <ozgecaylioglu@gmail.com>
2023-11-17 14:18:00 -0500

    added signup route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { Landing } from './screens/Landing/Landing'
 import { Route, Routes } from 'react-router'
 import { Layout } from './layout/Layout'
-
+import { SignUp } from './screens/SignUp/SignUp'
 function App() {
   return (
     <>
       <Layout>
         <Routes>
           <Route path='/' element={<Landing />} />
+          <Route path='/sign-up' element={<SignUp />} />
         </Routes>
       </Layout>
     </>
