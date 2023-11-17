48f98ade982a01f717ddff81aa6d3a7bb7a0b31f
GeoSauer <geoffrey.sauer89@gmail.com>
2023-11-17 16:56:52 -0700

    add optional change to match design at expense of UX

--- frontend/src/screens/SignUp/SignUpForm/SignUpForm.tsx
+++ frontend/src/screens/SignUp/SignUpForm/SignUpForm.tsx
@@ -325,10 +325,12 @@ export default function SignUpForm() {
                 will not sell your information!
               </Text>
             </HStack>
+
             <Button
               type='submit'
               isLoading={isSubmitting}
               isDisabled={!agree || !isValid}
+              // _disabled={{ opacity: 1, cursor: 'not-allowed' }} //matches design but impacts UX
               backgroundColor={agree && isValid ? '#EC993B' : '#ECEBEB'}
               _hover={{ background: agree && isValid ? '#EC993B' : '#ECEBEB' }}
               width={'448px'}
