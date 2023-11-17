1799e3c05f1eab9b404a140ebc348960e4805ea0
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-11-17 08:32:22 -0500

    Redo div

--- frontend/src/sign_up/Register.tsx
+++ frontend/src/sign_up/Register.tsx
@@ -4,19 +4,23 @@ import './Register.scss'
 export const Register: React.FC = () => {
     return (
         <div>
-        <h1>Join Bootcampr today.</h1>
-        <h2>Get the experience. Get the job.</h2>
-        <p>First name</p>
-        <p>Last name</p>
-        <p>Email address (ex. jeanine@bootcampr.io)</p>
-        <p>Password (Min 8 characters, 1 upper, 1 lower, 1 symbol)</p>
-        <p>Re-enter password</p>
-        <p>I agree to receive email notification(s). We will only
-            send emails with important information, like project start 
-            dates. We will not sell your information!
-        </p>
+            <div id="wrap">
+                <div id="content">
+                    <h1>Join Bootcampr today.</h1>
+                    <h3>Get the experience. Get the job.</h3>
+                </div>
+            </div>
+            <p>First name</p>
+            <p>Last name</p>
+            <p>Email address (ex. jeanine@bootcampr.io)</p>
+            <p>Password (Min 8 characters, 1 upper, 1 lower, 1 symbol)</p>
+            <p>Re-enter password</p>
+            <p>I agree to receive email notification(s). We will only
+                send emails with important information, like project start
+                dates. We will not sell your information!
+            </p>
         </div>
-        
+
     )
 }
 
