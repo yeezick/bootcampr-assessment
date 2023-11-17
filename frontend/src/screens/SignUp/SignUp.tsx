fdf88fdbcaf59ae2b44d859beb5a9cafb62597a6
centipedejam <jheimburge@gmail.com>
2023-11-17 14:36:42 -0800

    style SignUp header, import Inter font

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -1,6 +1,13 @@
+import './SignUp.scss';
+
 export const SignUp: React.FC = () => {
     return (
-        <div>SignUp</div>
+        <div className="SignUp">
+            <header>
+                <h2>Join Bootcampr today.</h2>
+                <h4>Get the experience. Get the job.</h4>
+            </header>
+        </div>
     );
 
 }
