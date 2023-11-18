4b88bb2c6da787d44b17079faac5101eed016636
GeoSauer <geoffrey.sauer89@gmail.com>
2023-11-17 17:38:41 -0700

    refactor validationSchema into separate file

--- frontend/src/screens/SignUp/SignUpForm/SignUpForm.tsx
+++ frontend/src/screens/SignUp/SignUpForm/SignUpForm.tsx
@@ -22,7 +22,7 @@ import {
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { createUser } from 'utils/userController'
-import * as Yup from 'yup'
+import { validationSchema } from './validationSchema'
 
 type Values = {
   firstName: string
@@ -39,37 +39,6 @@ const initialValues = {
   password: '',
   confirmPassword: '',
 }
-
-const validationSchema = Yup.object().shape({
-  firstName: Yup.string().required('First name is required'),
-  lastName: Yup.string().required('Last name is required'),
-  email: Yup.string()
-    .email('Invalid email')
-    .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Invalid email')
-    .required('Email is required'),
-  password: Yup.string()
-    .required('Password is required')
-    .test('uppercase', 'Choose a stronger password', function (value) {
-      return /[A-Z]/.test(value)
-    })
-    .test('lowercase', 'Choose a stronger password', function (value) {
-      return /[a-z]/.test(value)
-    })
-    .test('number', 'Choose a stronger password', function (value) {
-      return /\d/.test(value)
-    })
-    .test('minLength', 'Choose a stronger password', function (value) {
-      return value.length >= 8
-    }),
-  confirmPassword: Yup.string()
-    .required('Please re-enter your password')
-    .matches(/[A-Z]/, 'Choose a stronger password')
-    .matches(/[a-z]/, 'Choose a stronger password')
-    .matches(/\d/, 'Choose a stronger password')
-    .min(8)
-
-    .oneOf([Yup.ref('password'), null], 'Passwords must match'),
-})
 
 export default function SignUpForm() {
   const [agree, setAgree] = useState(false)
