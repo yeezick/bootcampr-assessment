f7af625b481ff1e2363c8cba0e49f9d3ee771291
johnfinley <johnfinleybremerton@gmail.com>
2023-11-18 15:44:11 -0700

    Imported SignUp.scss and created elements for header information

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import './SignUp.scss'
 
 export const SignUp: React.FC = () => {
-  return <div>SignUp</div>
+  return (
+    <div className='signup-container'>
+      <div className='header-container'>
+        <h2>Join Bootcampr today.</h2>
+        <h4>Get the experience. Get the job.</h4>
+      </div>
+    </div>
+  )
 }
