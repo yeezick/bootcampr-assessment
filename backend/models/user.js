050667f51e7b6e04e13ede10d12a095d6ac3262a
rica213 <25f5k252l@mozmail.com>
2023-11-18 21:28:14 +0300

    Create user model

---
+++ backend/models/user.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
