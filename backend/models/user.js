2ae77338be1285212537c86c522e374aefc59154
rica213 <25f5k252l@mozmail.com>
2023-11-18 21:31:01 +0300

    Create a custom static method for signup

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
@@ -22,5 +23,65 @@ const userSchema = new Schema({
     select: false,
   },
 });
+
+userSchema.statics.signup = async function({
+  firstName,
+  lastName,
+  email,
+  password,
+}) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use.");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  try {
+    const user = await this.create({
+      firstName,
+      lastName,
+      email,
+      password: hash,
+    });
+
+    return user;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw new Error("Failed to create user.");
+  }
+};
+
+userSchema.statics.signup = async function({
+  firstName,
+  lastName,
+  email,
+  password,
+}) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use.");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  try {
+    const user = await this.create({
+      firstName,
+      lastName,
+      email,
+      password: hash,
+    });
+
+    return user;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw new Error("Failed to create user.");
+  }
+};
 
 export const User = mongoose.model("User", userSchema);
