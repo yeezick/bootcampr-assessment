a3f71aef46c8f86dde2231c1c013c22e5f0bdf15
johnfinley <johnfinleybremerton@gmail.com>
2023-11-18 16:44:09 -0700

    Updated code to use absolute and relative positioning to better conform to Figma design

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -9,33 +9,31 @@ export const SignUp: React.FC = () => {
         <h2>Join Bootcampr today.</h2>
         <h4>Get the experience. Get the job.</h4>
       </div>
-      <div className='content-container'>
+      <div className='image-container'>
         <img className='notes-hexagon' src={Notes} alt='Notetaker' />
-        <form className='form-container'>
-          <div className='input-container'>
-            <label>First Name</label>
-            <input type='text' name='firstName' />
-          </div>
-          <div className='input-container'>
-            <label>Last Name</label>
-            <input type='text' name='lastName' />
-          </div>
-          <div className='input-container'>
-            <label>Email address (ex. jeanine@bootcampr.io)</label>
-            <input type='email' name='email' />
-          </div>
-          <div className='input-container'>
-            <label>
-              Password (Min 8 characters, 1 upper, 1 lower, 1 symbol)
-            </label>
-            <input type='password' name='password' />
-          </div>
-          <div className='input-container'>
-            <label>Re-enter Password</label>
-            <input type='password' name='passwordConfirmation' />
-          </div>
-        </form>
       </div>
+      <form className='form-container'>
+        <div className='input-container'>
+          <label>First Name</label>
+          <input type='text' name='firstName' />
+        </div>
+        <div className='input-container'>
+          <label>Last Name</label>
+          <input type='text' name='lastName' />
+        </div>
+        <div className='input-container'>
+          <label>Email address (ex. jeanine@bootcampr.io)</label>
+          <input type='email' name='email' />
+        </div>
+        <div className='input-container'>
+          <label>Password (Min 8 characters, 1 upper, 1 lower, 1 symbol)</label>
+          <input type='password' name='password' />
+        </div>
+        <div className='input-container'>
+          <label>Re-enter Password</label>
+          <input type='password' name='passwordConfirmation' />
+        </div>
+      </form>
     </div>
   )
 }
