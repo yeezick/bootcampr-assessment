62c78ef10468633d01997c4cb46fa9058e8b05f7
rica213 <25f5k252l@mozmail.com>
2023-11-20 20:59:06 +0300

    Add congrats component to the route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Landing } from './screens/Landing/Landing'
 import { Route, Routes } from 'react-router'
 import { Layout } from './layout/Layout'
 import { SignUp } from 'screens/SignUp/SignUp'
+import Congrats from 'screens/Congrats/Congrats'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/congrats' element={<Congrats />} />
         </Routes>
       </Layout>
     </>
