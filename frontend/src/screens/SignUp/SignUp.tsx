f255d28ced16240453442c877c8225b804ee09a3
ajosephs1 <77748724+ajosephs1@users.noreply.github.com>
2023-11-20 13:43:11 -0500

    Initial commit

---
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './SignUp.scss'
+
+export const SignUp: React.FC = () => {
+
+  return (
+    <div className='landing-container'>
+      
+    </div>
+  )
+}
+
+
