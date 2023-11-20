33090c9fa3fa3ff80c72feb597f84bd471c59427
Andrea Cano <andreacanogisbert@gmail.com>
2023-11-20 10:31:57 -0800

    first commit - includes initial backend setup

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import "dotenv/config.js";
 
 // Define your connection URI here
-// const MONGODB_URI =
+const MONGODB_URI = process.env.MONGODB_URI
 
 mongoose.set("strictQuery", false);
 mongoose.set("returnOriginal", false); //for findByAndUpdate to return a reference to object at location
