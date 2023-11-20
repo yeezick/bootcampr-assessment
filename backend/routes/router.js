854cabe585c54b514812f4bbbae341d729b3ee2d
Anthony Lo <87.anthonylo@gmail.com>
2023-11-20 09:48:35 -0800

    changed routes to specify api routes, clean up code

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -7,15 +7,11 @@ const router = Router();
 // Hello World!
 router.get("/hello-world", getHelloWorld);
 
-router.get("/user", userController.getUser, (req, res) => {
+router.get("api/user", userController.getUser, (req, res) => {
   return res.status(200).json(res.locals.user);
 });
 
-router.get("/users", userController.getUser, (req, res) => {
-  return res.status(200).json(res.locals.users);
-});
-
-router.post("/user", userController.addUser, (req, res) => {
+router.post("api/user", userController.addUser, (req, res) => {
   return res.status(200).json("user created");
 });
 
