f2ab571f636c0c5f17951c49f73b67a86c906c31
GeoSauer <geoffrey.sauer89@gmail.com>
2023-11-21 11:18:20 -0600

    add SignUpForm test

--- frontend/src/__tests__/SignUp.spec.tsx
+++ frontend/src/__tests__/SignUp.spec.tsx
@@ -1,15 +1,28 @@
 import { render, screen } from '__tests__/customRender'
 import SignUp from 'screens/SignUp/SignUp'
+import SignUpForm from 'screens/SignUp/SignUpForm/SignUpForm'
 
-test('render SignUp screen', () => {
-  render(<SignUp />)
+describe('SignUp screen', () => {
+  test('render SignUp screen', () => {
+    render(<SignUp />)
 
-  expect(screen.getByText(/Join Bootcampr today./i)).toBeInTheDocument()
-  expect(
-    screen.getByText(/Get the experience. Get the job./i)
-  ).toBeInTheDocument()
-  expect(
-    screen.getByAltText('A person writing in a notebook.')
-  ).toBeInTheDocument()
-  expect(screen.getByRole('form')).toBeInTheDocument()
+    expect(screen.getByText('Join Bootcampr today.')).toBeInTheDocument()
+    expect(
+      screen.getByText('Get the experience. Get the job.')
+    ).toBeInTheDocument()
+    expect(
+      screen.getByAltText('A person writing in a notebook.')
+    ).toBeInTheDocument()
+    expect(screen.getByRole('form')).toBeInTheDocument()
+  })
+
+  test('render SignUpForm', () => {
+    render(<SignUpForm />)
+
+    expect(screen.getByLabelText('First name')).toBeInTheDocument()
+    expect(screen.getByLabelText('Last name')).toBeInTheDocument()
+    expect(screen.getByLabelText(/Email/i)).toBeInTheDocument()
+    expect(screen.getByLabelText('Password')).toBeInTheDocument()
+    expect(screen.getByLabelText('Re-enter password')).toBeInTheDocument()
+  })
 })
