9fa6beb84ed6febe371840b31f334a6a70f1e407
rica213 <25f5k252l@mozmail.com>
2023-11-22 00:38:21 +0300

    Adjust test for rendering field

--- frontend/src/__tests__/SignUpForm.spec.tsx
+++ frontend/src/__tests__/SignUpForm.spec.tsx
@@ -4,13 +4,18 @@ import { render, screen } from './customRender'
 describe('SignUpForm', () => {
   test('renders form fields and the button submit', () => {
     render(<SignUp />)
+
     expect(screen.getByLabelText(/first name/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/last name/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/email address/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
+    expect(
+      screen.getByLabelText(
+        /Password \(Minimum 8 characters, 1 uppercase, 1 lowercase, 1 symbol\)/i
+      )
+    ).toBeInTheDocument()
     expect(screen.getByLabelText(/re-enter password/i)).toBeInTheDocument()
     expect(
-      screen.getByLabelText(/email notification agreement/i)
+      screen.getByLabelText(/I agree to receive email notification/i)
     ).toBeInTheDocument()
     expect(screen.getByText(/sign up/i)).toBeInTheDocument()
   })
