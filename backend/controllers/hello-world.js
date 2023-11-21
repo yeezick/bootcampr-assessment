bf0fed21a4d50ad57e4bd38ceece8cc5aa509ae6
thiago bueno garcia <thiagobuenogarcia1@gmail.com>
2023-11-20 20:57:55 -0500

    connection set for createUser ASYNC call

--- backend/controllers/hello-world.js
+++ backend/controllers/hello-world.js
@@ -1,3 +1,15 @@
+import bcrypt from 'bcrypt';
+import createUser from '../models/user.js';
+
 export const getHelloWorld = (req, res) => {
   res.status(200).json("Hello World!");
 };
+
+export const createUser = async (req, res) => {
+  try {
+    const user = await createUser(req.body);
+    res.status(200).json({ message: 'User created', user: createUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
