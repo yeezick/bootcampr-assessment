11e717a64de35a5e24f4c9d58531d26099f4e72e
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-11-21 09:10:47 -0800

    wip: added hide/unhide password

--- frontend/src/screens/Signup/Signup.tsx
+++ frontend/src/screens/Signup/Signup.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FilledInput, FormControl, IconButton, InputAdornment } from '@mui/material';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+
 import signup_img from '../../assets/signup_img.png';
 import './Signup.scss';
 
 type Props = {}
 
 export const Signup = (props: Props) => {
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
+
+    const handleClickShowPassword = () => setShowPassword(show => !show);
+    const handleClickshowConfirmPassword = () => setShowConfirmPassword(show => !show);
+
+    const handleMouseDownPassword = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+    }
+    const handleMouseDownConfirmPassword = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+    }
+
     return (
         <div className='signup-container'>
             <div className="header">
@@ -27,19 +44,92 @@ export const Signup = (props: Props) => {
                             <input type="email" name='email' />
                         </label>
                         <div className="passwordBox">
-                            <label>Password
-                                <input type="password" name='password' />
-                                <div>
-                                    <p>1 uppercase</p>
-                                    <p>1 lowercase</p>
-                                    <p>1 number</p>
-                                    <p>Minimum 8 characters</p>
-                                </div>
-                            </label>
+                            <FormControl
+                                fullWidth
+                                variant='filled'
+                            >
+                                <label>Password</label>
+                                {/* <InputLabel htmlFor='password'>Password</InputLabel> */}
+                                <FilledInput
+                                    id='password'
+                                    type={showPassword ? 'text' : 'password'}
+                                    endAdornment={
+                                        <InputAdornment position='end'>
+                                            <IconButton
+                                                aria-label='toggle password visibility'
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}
+                                                sx={{
+                                                    bgcolor: "rgba(236, 235, 235, 1)",
+                                                    borderRadius: "4px",
+                                                    height: "36px",
+                                                }}
+                                            >
+                                                {showPassword ? <VisibilityOffOutlinedIcon /> : <VisibilityOutlinedIcon />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                    size='small'
+                                    disableUnderline={true}
+                                    sx={{
+                                        bgcolor: "rgba(236, 235, 235, 1)",
+                                        borderRadius: "4px",
+                                        height: "36px",
+                                        mt: 0.5,
+                                        mb: 0,
+                                        '& .MuiFilledInput-input': {
+                                            p: "10px",
+                                            fontSize: "12px"
+                                        }
+                                    }}
+                                />
+                            </FormControl>
+                            <div className='passwordValidation'>
+                                <p>1 uppercase</p>
+                                <p>1 lowercase</p>
+                                <p>1 number</p>
+                                <p>Minimum 8 characters</p>
+                            </div>
                         </div>
-                        <label>Re-enter password
-                            <input type="password" name='confirmPassword' />
-                        </label>
+                        <FormControl
+                            fullWidth
+                            variant='filled'
+                        >
+                            <label>Re-enter password</label>
+                            <FilledInput
+                                id='confirmPassword'
+                                type={showConfirmPassword ? 'text' : 'password'}
+                                endAdornment={
+                                    <InputAdornment position='end'>
+                                        <IconButton
+                                            aria-label='toggle password visibility'
+                                            onClick={handleClickshowConfirmPassword}
+                                            onMouseDown={handleMouseDownConfirmPassword}
+                                            sx={{
+                                                bgcolor: "rgba(236, 235, 235, 1)",
+                                                borderRadius: "4px",
+                                                height: "36px",
+                                            }}
+                                        >
+                                            {showConfirmPassword ? <VisibilityOffOutlinedIcon /> : <VisibilityOutlinedIcon />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                size='small'
+                                disableUnderline={true}
+                                sx={{
+                                    bgcolor: "rgba(236, 235, 235, 1)",
+                                    borderRadius: "4px",
+                                    height: "36px",
+                                    mt: 0.5,
+                                    mb: 0,
+                                    '& .MuiFilledInput-input': {
+                                        p: "10px",
+                                        fontSize: "12px"
+                                    }
+                                }}
+                            />
+                        </FormControl>
                         <div className='signup-checkbox'>
                             <input type="checkbox" id='notifications' />
                             <label htmlFor="notifications">
