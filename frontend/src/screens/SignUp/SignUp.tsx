2b4da11f021ff6daed8184c5a424e8e9bb30f5b9
ajosephs1 <77748724+ajosephs1@users.noreply.github.com>
2023-11-20 22:56:34 -0500

    Add SignUp screen and initial components

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react'
+import Container from '@mui/material/Container'
+import Typograhy from '@mui/material/Typography'
 import './SignUp.scss'
+import hexagon from '../../assets/Hexagon.svg'
 
 export const SignUp: React.FC = () => {
-
   return (
-    <div className='landing-container'>
-      
-    </div>
+    <Container>
+      <div className='landing-container'>
+        <div className='signup-container'>
+          <Typograhy variant='h3' component='h1'>Join Bootcampr today.</Typograhy>
+          <Typograhy variant='h5' component='h2'>Get the experience. Get the Job.</Typograhy>
+        </div>
+        <div className='flex-container'>
+          <img src={hexagon} alt='hand writing with pen' />
+        </div>
+        <div className='form-container'></div>
+      </div>
+    </Container>
   )
 }
-
-
