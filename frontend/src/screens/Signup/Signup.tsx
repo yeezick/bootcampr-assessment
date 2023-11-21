b2db046c9233e72178ceb4275228ceaaf46e6696
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-11-21 20:37:11 +0100

    Refactor formik form to use Formik, Field, Form, and ErrorMessage components

--- frontend/src/screens/Signup/Signup.tsx
+++ frontend/src/screens/Signup/Signup.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 import './Signup.scss'
 import { useNavigate } from 'react-router-dom'
-import { useFormik } from 'formik'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
 import * as yup from 'yup'
 import signup from 'assets/signup-image.png'
-import PasswordCriteriaMet from './PasswordCriteriaMet'
+// import PasswordCriteriaMet from './PasswordCriteriaMet'
 
 export const Signup: React.FC = () => {
-    const [errors, setErrors] = useState(null);
-    const [isPasswordHidden, setIsPasswordHidden] = useState(true);
-    const [isPasswordConfirmationHidden, setIsPasswordConfirmationHidden] = useState(true);
-    const [isPasswordFocus, setIsPasswordFocus] = useState(false);
-    const [isPasswordConfirmationFocus, setIsPasswordConfirmationFocus] = useState(false);
+    // const [errors, setErrors] = useState(null);
+    // const [isPasswordHidden, setIsPasswordHidden] = useState(true);
+    // const [isPasswordConfirmationHidden, setIsPasswordConfirmationHidden] = useState(true);
+    // const [isPasswordFocus, setIsPasswordFocus] = useState(false);
+    // const [isPasswordConfirmationFocus, setIsPasswordConfirmationFocus] = useState(false);
 
     const navigate = useNavigate();
 
@@ -19,24 +19,24 @@ export const Signup: React.FC = () => {
         email: '(ex. jeanine@bootcampr.io)',
     }
 
-    function handleClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
-        const id = event.target['id'] 
-        if (id === 'password-span') {
-        setIsPasswordHidden(isPasswordHidden => !isPasswordHidden);
-        } else {
-            setIsPasswordConfirmationHidden(isPasswordConfirmationHidden => !isPasswordConfirmationHidden)
-        }
-    }
+    // function handleClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+    //     const id = event.target['id'] 
+    //     if (id === 'password-span') {
+    //     setIsPasswordHidden(isPasswordHidden => !isPasswordHidden);
+    //     } else {
+    //         setIsPasswordConfirmationHidden(isPasswordConfirmationHidden => !isPasswordConfirmationHidden)
+    //     }
+    // }
 
-    const handlePasswordFocus = (): void => {
-        setIsPasswordFocus(true)
-    }
+    // const handlePasswordFocus = (): void => {
+    //     setIsPasswordFocus(true)
+    // }
 
-    const handlePasswordConfirmationFocus = (): void => {
-        setIsPasswordConfirmationFocus(true)
-    }
+    // const handlePasswordConfirmationFocus = (): void => {
+    //     setIsPasswordConfirmationFocus(true)
+    // }
 
-    const formSchema = yup.object().shape({
+    const validationSchema = yup.object().shape({
         firstName: yup.string().required('First name is required.'),
         lastName: yup.string().required('Last name is required.'),
         email: yup.string().required('Email address is required.')
@@ -46,46 +46,148 @@ export const Signup: React.FC = () => {
         passwordConfirmation: yup.string()
         .oneOf([yup.ref('password'), null], 'Passwords must match.')
         .required('Please re-enter password.'),
+        checkbox: yup.bool().oneOf([true], 'You must agree to the terms.')
     });
 
-    const formik = useFormik({
-        initialValues: {
+    const initialValues = {
             firstName: "",
             lastName: "",
             email: "",
             password: "",
             passwordConfirmation: "",
             checkbox: false,
-        },
-        validationSchema: formSchema,
-        onSubmit: async (values) => {
-            try {
-                setErrors([]);
+        };
+    const onSubmit = (values: any) => {
+        console.log(values);
+            // try {
+            //     setErrors([]);
 
-                const response = await fetch('', {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(values),
-                });
-                if (response.ok) {
-                    navigate("/confirmation")
-                } else {
-                    const errorData = await response.json();
-                    setErrors(errorData);
-                }
-            } catch (error) {
-                console.error("Error occured during signup.", error);
-                setErrors([{message: "Error occured during signup."}]);
-            }
-        }
-    });
-
-    console.log(formik.touched.passwordConfirmation)
+            //     const response = await fetch('', {
+            //         method: "POST",
+            //         headers: {
+            //             "Content-Type": "application/json",
+            //         },
+            //         body: JSON.stringify(values),
+            //     });
+            //     if (response.ok) {
+            //         navigate("/confirmation")
+            //     } else {
+            //         const errorData = await response.json();
+            //         setErrors(errorData);
+            //     }
+            // } catch (error) {
+            //     console.error("Error occured during signup.", error);
+            //     setErrors([{message: "Error occured during signup."}]);
+            // }
+        };
 
   return (
     <div className='signup-container'>
+        <div className='header-container'>
+        <h2>Join Bootcampr today.</h2>
+        <h4>Get the experience. Get the job.</h4>
+      </div>
+      <div className='body-container'>
+      <div className='image-container'>
+        <div className='image-wrapper'>
+        <img src={signup} alt='Hand drawing diagram with black pen'></img>
+        </div>
+      </div>
+<Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+    {/* <div className='form-container'> */}
+    {({ isValid, dirty }) => (
+      <Form>
+        <div className='form-wrapper'>
+          <div className='form-item'>
+            <label htmlFor='firstName' className='label'>First name</label>
+            <Field
+              type='text'
+              id='firstName'
+              autoComplete='off'
+              name='firstName'
+              className='input'
+            />
+            <ErrorMessage name='firstName' component='div' className='errors' />
+          </div>
+
+          <div className='form-item'>
+            <label htmlFor='lastName' className='label'>Last name</label>
+            <Field
+              type='text'
+              id='lastName'
+              autoComplete='off'
+              name='lastName'
+              className='input'
+            />
+            <ErrorMessage name='lastName' component='div' className='errors' />
+          </div>
+
+          <div className='form-item'>
+            <label htmlFor='email' className='label'>Email address</label>
+            <Field
+              type='text'
+              id='email'
+              autoComplete='off'
+              name='email'
+              className='input'
+            />
+            <ErrorMessage name='email' component='div' className='errors' />
+          </div>
+
+          <div className='form-item'>
+            <label htmlFor='password' className='label'>Password</label>
+            <Field
+              type='text'
+              id='password'
+              autoComplete='off'
+              name='password'
+              className='input'
+            />
+            <ErrorMessage name='password' component='div' className='errors' />
+          </div>
+
+          <div className='form-item'>
+            <label htmlFor='passwordConfirmation' className='label'>Re-enter password</label>
+            <Field
+              type='text'
+              id='passwordConfirmation'
+              autoComplete='off'
+              name='passwordConfirmation'
+              className='input'
+            />
+            <ErrorMessage name='passwordConfirmation' component='div' className='errors' />
+          </div>
+
+          <div className='form-item-checkbox'>
+            <div className='checkbox-container'>
+              <Field
+                type='checkbox'
+                id='checkbox'
+                name='checkbox'
+                className='input-checkbox'
+              />
+              <span className='checkmark'></span>
+            </div>
+            <label htmlFor='checkbox' className='label'>
+              I agree to receive email notification(s). We will only send emails with important information,
+              like project start dates. We will not sell your information!
+            </label>
+            <ErrorMessage name='checkbox' component='div' className='errors' />
+          </div>
+
+          <button type='submit' className={`submit-form ${isValid && dirty ? 'valid' : 'invalid'}`} children="Sign up" />
+        </div>
+      </Form>
+    )}
+      {/* </div> */}
+    </Formik>
+    </div>
+{/* 
+
       <div className='header-container'>
         <h2>Join Bootcampr today.</h2>
         <h4>Get the experience. Get the job.</h4>
@@ -209,11 +311,11 @@ export const Signup: React.FC = () => {
                     </div>
                 <div className='error'>
                 {errors}</div>
-                <button type='submit' className={!formik.errors ? 'confirm' : null} children="Sign up" />
+                <button type='submit' className='submit-form' children="Sign up" />
             </div>
         </form>
       </div>
-      </div>
+      </div> */}
     </div>
   )
 }
