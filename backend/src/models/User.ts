f8c0fe1bdf3fb4d43f5c4d2d38dcb1be33f33e2c
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-11-21 01:17:48 -0800

    corrected user model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -10,7 +10,8 @@ interface IUser {
 const UserSchema = new mongoose.Schema<IUser>({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true, select: false }
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, select: false }
 }, { timestamps: true });
 
 const User = mongoose.model<IUser>("User", UserSchema);
