7b5959476d20a38aeb0039b45d489bf055d969f3
ozge <ozgecaylioglu@gmail.com>
2023-11-21 11:32:57 -0500

    added inline button styling

--- frontend/src/components/Button/CustomButton.tsx
+++ frontend/src/components/Button/CustomButton.tsx
@@ -1,18 +1,40 @@
-import React, { useState } from 'react'
+import { FC } from 'react'
 import { Button } from '@mui/material'
 import './CustomButton.scss' // Import your SCSS file for styles
 
 interface CustomButtonProps {
   text: string
+  disabled?: boolean
   onClick: () => void
+  type?: 'contained' | 'text' | 'outlined'
 }
 
-export const CustomButton: React.FC<CustomButtonProps> = ({
+export const CustomButton: FC<CustomButtonProps> = ({
   text,
+  disabled,
+  type = 'contained',
   onClick,
 }) => {
   return (
-    <Button className='custom-button' onClick={onClick}>
+    <Button
+      // className='custom-button'
+      sx={{
+        backgroundColor: '#fa9413',
+        border: 'none',
+        color: '#313335',
+        fontSize: '16px',
+        textTransform: 'none',
+        textDecoration: 'none',
+        boxShadow: 'none',
+        '&:hover': {
+          backgroundColor: '#fba52d',
+          boxShadow: 'none',
+        },
+      }}
+      onClick={onClick}
+      variant={type}
+      disabled={disabled}
+    >
       {text}
     </Button>
   )
