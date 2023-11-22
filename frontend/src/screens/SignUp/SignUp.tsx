09da9e5586de71357e03cdd18e74e34e5628ed08
Eliot Gronstal <eliot.lauren@gmail.com>
2023-11-21 17:37:22 -0800

    add wrapper to SignUp.scss for styling.

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -8,14 +8,15 @@ export const SignUp: React.FC = () => {
       <div className='text-banner'>
         <h2>Join Bootcampr Today</h2>
         <h4>Get the experience. Get the job.</h4>
-        </div>
-        <div className='img-container'>
-          <img
-            src={Rectangle}
-            alt='A hand holding a pen and writing on paper'
-          />
-        </div>
-      <SignUpForm />
+      </div>
+      <div className="wrapper">
+      <div className='img-container'>
+        <img src={Rectangle} alt='A hand holding a pen and writing on paper' />
+      </div>
+      <div className='sign-up-container'>
+        <SignUpForm />
+      </div>
+      </div>
     </>
   )
 }
