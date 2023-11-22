6f84889f0089db5c76478b4ab396ad7386142df0
johnfinley <johnfinleybremerton@gmail.com>
2023-11-22 16:49:20 -0700

    Create Successful SignUp page and stylesheet

---
+++ frontend/src/screens/SignUp/SignUpSuccess.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SignUpSuccess.scss'
+
+function SignUpSuccess() {
+  return (
+    <div className='signup-success-container'>
+      <div className='text-container'>
+        <h2>Congrats on making it to the end! </h2>
+      </div>
+    </div>
+  )
+}
+
+export default SignUpSuccess
