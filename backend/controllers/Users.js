77715e614047e09659561e1bd3b42ca12bb9715d
johnfinley <johnfinleybremerton@gmail.com>
2023-11-22 16:25:28 -0700

    Created Users controller and put route methods there

---
+++ backend/controllers/Users.js
@@ -0,0 +1,50 @@
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/User.js";
+
+export const checkEmail = async (req, res) => {
+  try {
+    const email = req.params.email;
+    const user = await UserModel.findOne({ email });
+
+    if (user) {
+      res.json({ message: "Email already exists!" });
+    } else {
+      res.json({ message: "Email doesn't exist" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const signUp = async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    userName,
+    email,
+    password,
+    passwordConfirmation,
+  } = req.body;
+
+  if (password !== passwordConfirmation) {
+    res.json({ message: "Passwords do not match!" });
+  }
+
+  const user = await UserModel.findOne({ userName });
+  if (user) {
+    res.json({ message: "Username already exists!" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({
+    firstName,
+    lastName,
+    userName,
+    email,
+    password: hashedPassword,
+  });
+  await newUser.save();
+
+  res.json({ message: "User Registered Successfully" });
+};
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,55 +1,10 @@
 import { Router } from "express";
-import bcrypt from "bcrypt";
-import { UserModel } from "../models/Users.js";
+import { signUp, checkEmail } from "../controllers/Users.js";
 
 const router = Router();
 
-router.get("/email-check/:email", async (req, res) => {
-  try {
-    const email = req.params.email;
-    const user = await UserModel.findOne({ email });
+router.get("/email-check/:email", checkEmail);
 
-    if (user) {
-      res.json({ message: "Email already exists!" });
-    } else {
-      res.json({ message: "Email doesn't exist" });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
-router.post("/register", async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    userName,
-    email,
-    password,
-    passwordConfirmation,
-  } = req.body;
-
-  if (password !== passwordConfirmation) {
-    res.json({ message: "Passwords do not match!" });
-  }
-
-  const user = await UserModel.findOne({ userName });
-  if (user) {
-    res.json({ message: "Username already exists!" });
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const newUser = new UserModel({
-    firstName,
-    lastName,
-    userName,
-    email,
-    password: hashedPassword,
-  });
-  await newUser.save();
-
-  res.json({ message: "User Registered Successfully" });
-});
+router.post("/register", signUp);
 
 export default router;
