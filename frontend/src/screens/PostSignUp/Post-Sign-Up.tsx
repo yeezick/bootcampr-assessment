f63617cd18590e568a89508cb51006a087debb9c
oneminch <27751688+oneminch@users.noreply.github.com>
2023-11-21 20:57:35 -0500

    feat: add confetti to post sign up page

--- frontend/src/screens/PostSignUp/Post-Sign-Up.tsx
+++ frontend/src/screens/PostSignUp/Post-Sign-Up.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Confetti from 'components/Confetti/Confetti'
 import './Post-Sign-Up.scss'
 
 const PostSignUp: React.FC = () => {
@@ -9,6 +9,7 @@ const PostSignUp: React.FC = () => {
         <h1>You have successfully sign up.</h1>
         <p>Next Step: Please Verify Your Email (not really)</p>
       </div>
+      <Confetti />
       <div className='body-container'>
         <img
           src='https://media.tenor.com/GfSX-u7VGM4AAAAC/coding.gif'
