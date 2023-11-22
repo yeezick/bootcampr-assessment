f0ae3295bb66d3284f5f23113fecd6b48ff2555f
Vanessa Wharton <vanessa.primetime@gmail.com>
2023-11-22 15:30:10 -0600

    Added fun successful registration page styling.

--- frontend/src/screens/Register/SuccessPage.tsx
+++ frontend/src/screens/Register/SuccessPage.tsx
@@ -1,11 +1,40 @@
 import React, { useState } from 'react'
-// import './SuccessPage.scss'
+import Confetti from 'react-confetti'
+import './SuccessPage.scss'
 
 export const SuccessPage: React.FC = () => {
 
     return (
-        <div>
-            Congrats! You are registered!
+        <div className='success-page'>
+        <Confetti />
+        <div className='content'>
+            <svg width="400" height="400">
+            <circle
+                fill="none"
+                stroke="#68E534"
+                stroke-width="20"
+                cx="200"
+                cy="200"
+                r="190"
+                strokeLinecap="round"
+                transform="rotate(-90 200 200)"
+                className="circle"
+            />
+            <polyline
+                fill="none"
+                stroke="#68E534"
+                points="88,214 173,284 304,138"
+                strokeWidth="24"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="tick"
+            />
+            </svg>
+            <center>
+                <h1>Congratulations!</h1>
+                <p>You are now registered.</p>
+            </center>
+        </div>
         </div>
     )
 }
