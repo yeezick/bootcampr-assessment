0b272c8c0bf4b5e7172f71c448a40e39cdcfb6e8
Felix Owolabi <owolabifelix78@gmail.com>
2023-11-22 10:53:32 +0100

    feat: added react-confetti to congrats screen

--- frontend/src/screens/Congrats/Congrats.tsx
+++ frontend/src/screens/Congrats/Congrats.tsx
@@ -3,10 +3,27 @@
 import React from 'react';
 import './Congrats.scss';
 import Icon from 'assets/welcome.gif';
+import Confetti from 'react-confetti';
 
 const Congrats:React.FC = () => {
+    // const confettiConfig: ConfettiConfig = {
+    //     angle: 90,
+    //     spread: 360,
+    //     startVelocity: 40,
+    //     elementCount: 50,
+    //     dragFriction: 0.12,
+    //     duration: 3000,
+    //     stagger: 0,
+    //     width: '10px',
+    //     height: '10px',
+    //     colors: ['#a864fd', '#29cdff', '#78ff44', '#ff718d', '#fdff6a'],
+    //   };
   return (
     <div className="onboarding-container">
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+        />
       <header>
         <h1>Congratulations on joining bootcampr🎉</h1>
         <p>Get the experience. Get the job.</p>
