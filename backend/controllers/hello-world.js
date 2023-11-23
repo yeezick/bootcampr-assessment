f17d6f7b92ab4de718c88ee9be4aeec0c8657b8e
Yufa <112290188+01001101CK@users.noreply.github.com>
2023-11-22 21:06:57 -0800

    setting up MongoDB: Create user model, Create controller to create new users, get all users, update existing users, and delete users

--- backend/controllers/hello-world.js
+++ backend/controllers/hello-world.js
@@ -1,3 +1,44 @@
-export const getHelloWorld = (req, res) => {
-  res.status(200).json("Hello World!");
+import User from '../models/user';
+
+
+export const createUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id);
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
