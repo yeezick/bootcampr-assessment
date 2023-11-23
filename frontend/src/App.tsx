866c7e0713cd96d033e89ea5f4fe2e3a10ec91ad
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-11-23 07:28:54 -0800

    added congrats route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Landing } from './screens/Landing/Landing'
 import { Signup } from './screens/Signup/Signup';
 import { Route, Routes } from 'react-router-dom'
 import { Layout } from './layout/Layout'
+import Congrats from 'screens/Congrats/Congrats';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='/sign-up' element={<Signup />} />
+          <Route path='/congrats' element={<Congrats />} />
         </Routes>
       </Layout>
     </>
