9bdd77b0c846f51619205b3c874e0a240585489c
nesbita <ariananesbit@gmail.com>
2023-11-23 13:22:00 -0500

    page is rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Landing } from './screens/Landing/Landing'
+import Signup from './Signup'
 import { Route, Routes } from 'react-router'
 import { Layout } from './layout/Layout'
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path='/' element={<Landing />} />
+          <Route path='/Signup' element={<Signup />} />
         </Routes>
       </Layout>
     </>
---
+++ frontend/src/Signup.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+// import { Landing } from './Landing';
+import { Component } from 'react'
+
+// export const Signup: React.FC = () => {
+function Signup () {
+    // const [helloResponse, setHelloResponse] = useState<string>('')
+    return (
+        <div className='container'>
+            <div className='imageOne'>
+                <img src='signup_image.jpg' alt='A hand writing on a piece of paper with a pen'></img>
+            </div>
+            <div className='formOne'>
+                <h3> the form goes here</h3>
+                {/* <ButtonContainer setHelloResponse={setHelloResponse} />
+          {helloResponse !== '' && helloResponse} */}
+            </div>
+        </div>
+
+// const ButtonContainer = ({ setHelloResponse }) => {
+//     const navigate = useNavigate()
+//     const handleSignUpButton = () => {
+//       navigate('/sign-up')
+//     }
+    );
+}
+export default Signup
