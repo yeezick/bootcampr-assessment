9ba01f9a52a188599f8dd924516f504e9b7b0a4b
Yufa <112290188+01001101CK@users.noreply.github.com>
2023-11-22 17:42:46 -0800

    frontend of the signup page and make it responsive

---
+++ frontend/src/screens/signup/Signup.tsx
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, TextField, InputLabel, Checkbox, FormControlLabel } from '@mui/material';
+import './Signup.scss';
+import Write from 'assets/Write.svg'
+
+export const Signup: React.FC = () => {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        agreedTerms: false,
+    });
+
+    const handleChange = (e) => {
+  
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+     
+    };
+
+
+    return (
+        <div className='signup-container'>
+            <header>
+                <h1>Join Bootcampr Today.</h1>
+                <h3>Get the experience. Get the job.</h3>
+            </header>
+
+            <div className='content-container'>
+                <div className='logo'>
+                    <img src={Write} alt="Logo" />
+                </div>
+
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <InputLabel htmlFor="firstName">First Name</InputLabel>
+                        <TextField
+                            type="text"
+                            id="firstName"
+                            name="firstName"
+                            value={formData.firstName}
+                            placeholder ='Jeanine'
+                            onChange={handleChange}
+                            variant="filled"
+                        />
+                    </div>
+
+                    <div>
+                        <InputLabel htmlFor="lastName">Last Name</InputLabel>
+                        <TextField
+                            type="text"
+                            id="lastName"
+                            name="lastName"
+                            placeholder='Bootcampr'
+                            value={formData.lastName}
+                            onChange={handleChange}
+                            variant="filled"
+                        />
+                    </div>
+
+                    <div>
+                        <InputLabel htmlFor="email">Email address (ex. jeanine@bootcampr.io) </InputLabel>
+                        <TextField
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder='jeanine@bootcampr.io'
+                            value={formData.email}
+                            onChange={handleChange}
+                            variant="filled"
+                        />
+                    </div>
+
+                    <div>
+                        <InputLabel htmlFor="password">Password (Min 8 characters, 1 upper, 1 lower, 1 symbol)</InputLabel>
+                        <TextField
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder='T'
+                            value={formData.password}
+                            onChange={handleChange}
+                            variant="filled"
+                        />
+                    </div>
+
+                    <div>
+                        <InputLabel htmlFor="confirmPassword">Re-enter Password</InputLabel>
+                        <TextField
+                            type="password"
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
+                            variant="filled"
+                        />
+                    </div>
+
+                    <div>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={formData.agreedTerms}
+                                    onChange={handleChange}
+                                    name="agreedTerms"
+                                />
+                            }
+                            label="I agree to receive email notification(s). We will only send 
+                            emails with important information, like project start dates. 
+                            We will not sell your information!"
+                        />
+                    </div>
+
+                    <Button type="submit" variant="contained" className="custom-button">
+                        Sign Up
+                    </Button>
+                </form>
+            </div>
+        </div>
+    );
+};
