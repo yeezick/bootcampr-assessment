5bd5cf8af1a092aa2d31f123a2c74c0d56909cae
noordibou <noor.dibou@hotmail.com>
2023-11-23 00:41:48 -0600

    fixed agreement issue & finished up styling

--- frontend/src/screens/Signup/Signup.tsx
+++ frontend/src/screens/Signup/Signup.tsx
@@ -1,87 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import signup from '../../assets/images/signup.png';
 import icon from '../../assets/images/icon.png';
 import './Signup.scss';
 
+const initialFormData = {
+  email: '',
+  password: '',
+  reEnterPassword: '',
+  firstName: '',
+  lastName: '',
+  agreement: false,
+};
+
 export const Signup: React.FC = () => {
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-        reEnterPassword: '',
-        firstName: '',
-        lastName: '',
-        agreement: false,
-    });
-    const [error, setError] = useState<string | null>(null);
-    const [showPassword, setShowPassword] = useState(false);
-    const [passwordTouched, setPasswordTouched] = useState(false);
-    const [passwordsMatch, setPasswordsMatch] = useState(false);
-    const navigate = useNavigate();
+  const [formData, setFormData] = useState(initialFormData);
+  const [error, setError] = useState<string | null>(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const [passwordTouched, setPasswordTouched] = useState(false);
+  const [passwordsMatch, setPasswordsMatch] = useState(false);
+  const [formComplete, setFormComplete] = useState(false);
+  const navigate = useNavigate();
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value, type } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: type === 'checkbox' ? (e.target as HTMLInputElement).checked : value,
-        }));
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, type, value } = e.target;
 
-        if (name === 'password' && !passwordTouched) {
-            setPasswordTouched(true);
-        }
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'checkbox' ? (e.target as HTMLInputElement).checked : value,
+    }));
 
-        if (name === 'reEnterPassword') {
-            setPasswordsMatch(value === formData.password);
-        }
-    };
+    if (name === 'password' && !passwordTouched) {
+      setPasswordTouched(true);
+    }
 
-    const handleBlur = () => {
-        // Reset passwordTouched to false when the password input loses focus
-        if (passwordTouched) {
-            setPasswordTouched(false);
-        }
+    if (name === 'reEnterPassword') {
+      setPasswordsMatch(value === formData.password);
+    }
+  };
 
-        // Reset passwordsMatch to false when the re-enter password input loses focus
-        if (passwordsMatch) {
-            setPasswordsMatch(false);
-        }
+  useEffect(() => {
+    const isFormComplete =
+      Object.values(formData).every((value) => value !== '') && formData.agreement;
+    setFormComplete(isFormComplete);
+  }, [formData]);
 
-    };
+  const handleBlur = () => {
+    if (passwordTouched) {
+      setPasswordTouched(false);
+    }
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+    if (passwordsMatch) {
+      setPasswordsMatch(false);
+    }
+  };
 
-        // Password matching validation
-        if (formData.password !== formData.reEnterPassword) {
-            setError('Passwords do not match.');
-            return;
-        }
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-        try {
-            const response = await axios.post('http://localhost:8001/sign-up', formData);
+    if (formData.password !== formData.reEnterPassword) {
+      setError('Passwords do not match.');
+      return;
+    }
 
-            if (response.status === 201) {
-                navigate('/success');
-                alert('User signed up successfully');
-            } else {
-                alert('Failed to sign up. Please try again.');
-            }
-        } catch (error) {
-            if (error.response.status === 409) {
-                setError('User already exists. Please choose a different email.');
-            } else if (error.response.status === 400) {
-                setError('Password does not meet complexity requirements');
-            } else {
-                console.error('Error:', error);
-                alert('An error occurred during signup.');
-            }
-        }
-    };
+    try {
+      const response = await axios.post('http://localhost:8001/sign-up', formData);
 
-    const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
-    };
+      if (response.status === 201) {
+        navigate('/success');
+      } else {
+        alert('Failed to sign up. Please try again.');
+      }
+    } catch (error) {
+      if (error.response.status === 409) {
+        setError('User already exists. Please choose a different email.');
+      } else {
+        console.error('Error:', error);
+        alert('An error occurred during signup.');
+      }
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
     return (
         <div className="container">
@@ -121,17 +124,17 @@ export const Signup: React.FC = () => {
                                 alt="icon"
                                 className="icon"
                                 onClick={togglePasswordVisibility}
-                                />
+                            />
                         </div>
 
-                                {passwordTouched && (
-                                    <div className="password-strength">
-                                        <span style={{ color: /[A-Z]/.test(formData.password) ? 'var(--seafoam)' : 'var(--rubarb)' }}>1 uppercase</span>
-                                        <span style={{ color: /[a-z]/.test(formData.password) ? 'var(--seafoam)' : 'var(--rubarb)' }}>1 lowercase</span>
-                                        <span style={{ color: /[!@#$%^&*(),.?":{}|<>]/.test(formData.password) ? 'var(--seafoam)' : 'var(--rubarb)' }}>1 symbol</span>
-                                        <span style={{ color: formData.password.length >= 8 ? 'var(--seafoam)' : 'var(--rubarb)' }}>Minimum 8 characters</span>
-                                    </div>
-                                )}
+                        {passwordTouched && (
+                            <div className="password-strength">
+                                <span style={{ color: /[A-Z]/.test(formData.password) ? 'var(--seafoam)' : 'var(--rubarb)' }}>1 uppercase</span>
+                                <span style={{ color: /[a-z]/.test(formData.password) ? 'var(--seafoam)' : 'var(--rubarb)' }}>1 lowercase</span>
+                                <span style={{ color: /[!@#$%^&*(),.?":{}|<>]/.test(formData.password) ? 'var(--seafoam)' : 'var(--rubarb)' }}>1 symbol</span>
+                                <span style={{ color: formData.password.length >= 8 ? 'var(--seafoam)' : 'var(--rubarb)' }}>Minimum 8 characters</span>
+                            </div>
+                        )}
 
                         <label htmlFor="reEnterPassword">Re-enter password</label>
                         <div className="password-container">
@@ -141,7 +144,7 @@ export const Signup: React.FC = () => {
                                 name="reEnterPassword"
                                 value={formData.reEnterPassword}
                                 onChange={handleChange}
-                                // onBlur={handleBlur}
+                                onBlur={handleBlur}
                                 required
                             />
                             <img
@@ -162,14 +165,19 @@ export const Signup: React.FC = () => {
                                 name="agreement"
                                 checked={formData.agreement}
                                 onChange={handleChange}
-                                required
                             />
                             <label htmlFor="agreement">
                                 I agree to receive email notification(s). We will only send emails with important information, like project start dates. We will not sell your information!
                             </label>
                         </div>
 
-                        <button type="submit">Sign Up</button>
+                        <button
+                            type="submit"
+                            style={{ backgroundColor: formComplete ? 'var(--apricot)' : 'var(--smoke)' }}
+                            disabled={!formComplete}
+                        >
+                            Sign Up
+                        </button>
                     </form>
                 </div>
             </div>
