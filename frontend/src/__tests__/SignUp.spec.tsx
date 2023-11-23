d5d41b6c77918d20d3fb29f39fcc2f2b30b39505
GeoSauer <geoffrey.sauer89@gmail.com>
2023-11-22 19:16:50 -0600

    add more test coverage

--- frontend/src/__tests__/SignUp.spec.tsx
+++ frontend/src/__tests__/SignUp.spec.tsx
@@ -1,28 +1,84 @@
-import { render, screen } from '__tests__/customRender'
+import { fireEvent, render, screen } from '__tests__/customRender'
+import { act } from 'react-dom/test-utils'
 import SignUp from 'screens/SignUp/SignUp'
 import SignUpForm from 'screens/SignUp/SignUpForm/SignUpForm'
 
 describe('SignUp screen', () => {
-  test('render SignUp screen', () => {
+  it('renders SignUp screen', () => {
     render(<SignUp />)
 
-    expect(screen.getByText('Join Bootcampr today.')).toBeInTheDocument()
-    expect(
-      screen.getByText('Get the experience. Get the job.')
-    ).toBeInTheDocument()
-    expect(
-      screen.getByAltText('A person writing in a notebook.')
-    ).toBeInTheDocument()
+    expect(screen.getByText(/join bootcampr/i)).toBeInTheDocument()
+    expect(screen.getByText(/the experience/i)).toBeInTheDocument()
+    expect(screen.getByAltText(/person writing/i)).toBeInTheDocument()
     expect(screen.getByRole('form')).toBeInTheDocument()
   })
 
-  test('render SignUpForm', () => {
+  it('renders SignUpForm', () => {
     render(<SignUpForm />)
 
-    expect(screen.getByLabelText('First name')).toBeInTheDocument()
-    expect(screen.getByLabelText('Last name')).toBeInTheDocument()
-    expect(screen.getByLabelText(/Email/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/first/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/last/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
     expect(screen.getByLabelText('Password')).toBeInTheDocument()
-    expect(screen.getByLabelText('Re-enter password')).toBeInTheDocument()
+    expect(screen.getByLabelText(/re-enter/i)).toBeInTheDocument()
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+    expect(screen.getByText(/agree to/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
   })
+
+  it('submits SignUpForm with valid data', async () => {
+    const { getByLabelText, getByText, getByRole } = render(<SignUpForm />)
+
+    fireEvent.change(getByLabelText(/first/i), {
+      target: { value: 'Test' },
+    })
+    fireEvent.change(getByLabelText(/last/i), {
+      target: { value: 'User' },
+    })
+    fireEvent.change(getByLabelText(/email/i), {
+      target: { value: 'test@user.com' },
+    })
+    fireEvent.change(getByLabelText('Password'), {
+      target: { value: 'Test1234' },
+    })
+    fireEvent.change(getByLabelText(/re-enter/i), {
+      target: { value: 'Test1234' },
+    })
+
+    fireEvent.click(getByRole('checkbox'))
+
+    fireEvent.click(getByText(/sign up/i))
+
+    act(() => {
+      jest.advanceTimersByTime(1000)
+    })
+
+    expect(window.location.pathname).toBe('/congrats')
+
+    jest.useRealTimers()
+  })
+
+  // it('renders invalid input errors', async () => {
+  //   render(<SignUpForm />)
+
+  //   act(() => {
+  //     fireEvent.focus(screen.getByLabelText(/first/i))
+  //   })
+
+  //   expect(screen.getByText('First name is required')).toBeInTheDocument()
+  // fireEvent.change(getByLabelText(/last/i), {
+  //   target: { value: 'User' },
+  // })
+  // fireEvent.change(getByLabelText(/email/i), {
+  //   target: { value: 'test@user.com' },
+  // })
+  // fireEvent.change(getByLabelText('Password'), {
+  //   target: { value: 'Test1234' },
+  // })
+  // fireEvent.change(getByLabelText(/re-enter/i), {
+  //   target: { value: 'Test1234' },
+  // })
+
+  // fireEvent.click(getByText(/sign up/i))
+  // })
 })
