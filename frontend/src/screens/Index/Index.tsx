ef21f91fc91e39609a8a7048c4db512297305082
Andrea Cano <andreacanogisbert@gmail.com>
2023-11-22 21:45:17 -0800

    Implement new index component along with css

---
+++ frontend/src/screens/Index/Index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Index.scss';
+import { FaRegSmileBeam } from "react-icons/fa";
+
+export const Index = () => {
+  return (
+    <div className='titles'>
+      <h1 className='success-title'>Congrats on making it to the end! </h1>
+
+      <div className='sub-title-container'>
+        <a className='website' href="https://andreacano.dev/" target="_blank" rel="webiste">
+          Hire me! 🫱🏼‍🫲🏾
+          
+        </a>
+      </div>
+
+    </div>
+  )
+}
+
