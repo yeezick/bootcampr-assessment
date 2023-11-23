67e42d0faf76fc271711c4899c60e4c5ac9d7c66
nesbita <ariananesbit@gmail.com>
2023-11-23 14:42:42 -0500

    form has been added and is almost complete

--- frontend/src/Signup.tsx
+++ frontend/src/Signup.tsx
@@ -2,27 +2,143 @@ import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 // import { Landing } from './Landing';
 import { Component } from 'react'
+// import './Signup.css'
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
-// export const Signup: React.FC = () => {
-function Signup () {
-    // const [helloResponse, setHelloResponse] = useState<string>('')
+function Copyright(props: any) {
     return (
-        <div className='container'>
-            <div className='imageOne'>
-                <img src='signup_image.jpg' alt='A hand writing on a piece of paper with a pen'></img>
-            </div>
-            <div className='formOne'>
-                <h3> the form goes here</h3>
-                {/* <ButtonContainer setHelloResponse={setHelloResponse} />
-          {helloResponse !== '' && helloResponse} */}
-            </div>
-        </div>
-
-// const ButtonContainer = ({ setHelloResponse }) => {
-//     const navigate = useNavigate()
-//     const handleSignUpButton = () => {
-//       navigate('/sign-up')
-//     }
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
     );
 }
+
+function Signup () {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+          email: data.get('email'),
+          password: data.get('password'),
+        });
+      };
+    return (
+      <Container component="main" maxWidth="xs">
+        <div className='imageOne'>
+            <img src='signup_image.jpg' alt='A hand writing on a piece of paper with a pen'></img>
+        </div>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+        
+          <Typography component="h1" variant="h5">
+            Join Bootcampr today.  
+          </Typography>
+          <Typography component="h2" variant="h5">
+            Get the experience.  Get the job.  
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password (Min 8 characters, 1 upper, 1 lower, 1 symbol"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Re-enter password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I agree to receive email notification(s).  We will only send emails with important information, like project start dates.  We will not sell your information!"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign up
+            </Button>
+            <Grid container justifyContent="flex-end">
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+  );
+}
+     
+
 export default Signup
