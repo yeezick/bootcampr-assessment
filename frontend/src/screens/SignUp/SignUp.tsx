ccca1601e85937b3471c130d09ddaad7323ff858
johnfinley <johnfinleybremerton@gmail.com>
2023-11-22 17:55:20 -0700

    Replaced JSX with corresponding components

--- frontend/src/screens/SignUp/SignUp.tsx
+++ frontend/src/screens/SignUp/SignUp.tsx
@@ -1,48 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignUp.scss'
 import Notes from 'assets/Notes.png'
+import SignUpHeader from './SignUpHeader'
+import SignUpImage from './SignUpImage'
+import SignUpForm from './SignUpForm'
 
 export const SignUp: React.FC = () => {
   return (
-    <div className='signup-container'>
-      <div className='header-container'>
-        <h2>Join Bootcampr today.</h2>
-        <h4>Get the experience. Get the job.</h4>
-      </div>
-      <div className='image-container'>
-        <img className='notes-hexagon' src={Notes} alt='Notetaker' />
-      </div>
-      <form className='form-container'>
-        <div className='input-container'>
-          <label>First Name</label>
-          <input type='text' name='firstName' />
+        <div className='signup-container'>
+          <SignUpHeader />
+          <SignUpImage image={Notes} />
+          <SignUpForm />
         </div>
-        <div className='input-container'>
-          <label>Last Name</label>
-          <input type='text' name='lastName' />
-        </div>
-        <div className='input-container'>
-          <label>Email address (ex. jeanine@bootcampr.io)</label>
-          <input type='email' name='email' />
-        </div>
-        <div className='input-container'>
-          <label>Password (Min 8 characters, 1 upper, 1 lower, 1 symbol)</label>
-          <input type='password' name='password' />
-        </div>
-        <div className='input-container'>
-          <label>Re-enter Password</label>
-          <input type='password' name='passwordConfirmation' />
-        </div>
-        <div className='agreement-box'>
-          <input className='checkbox' type='checkbox' />
-          <p>
-            I agree to receive email notification(s). We will only send emails
-            with important information, like project start dates. We will not
-            sell your information!
-          </p>
-        </div>
-        <button>Sign Up</button>
-      </form>
-    </div>
   )
 }
