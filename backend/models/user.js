131efa6453be7b4be97e454ab4cf05a2c85ea414
david2tosin <david2tosin@gmail.com>
2023-11-26 23:04:50 +0100

    connected to database created user model

---
+++ backend/models/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
